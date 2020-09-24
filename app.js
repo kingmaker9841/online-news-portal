3a5e2eeff43ddcb7b0b8a9443d72fc0ee7556cf7
Mayesh <josephhernandez9841@gmail.com>
2020-09-24 07:22:22 +0545

    npm init express, mongoose, cors, graphql

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+
+let PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`);
+})
