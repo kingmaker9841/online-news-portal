70030da044cba707942c2eedde134a4998b285fd
Mayesh <josephhernandez9841@gmail.com>
2020-09-24 07:28:29 +0545

    install express-graphql

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
+mongoose.connect(process.env.MONGO_URI, 
+    {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
+        if (err) return console.log(err);
+        console.log("Mongoose Connected...");
+    })
 
 let PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
